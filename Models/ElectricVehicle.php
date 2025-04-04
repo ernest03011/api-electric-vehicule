6936fc44d815d79156989b75f4cd37e98e87d2ce
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-03 23:46:53 -0400

    Add a class for the Electric Vehicles model

---
+++ Models/ElectricVehicle.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ElectricVehicle extends Model
+{
+
+  protected $table = "electric_vehicle_population_data";
+}
