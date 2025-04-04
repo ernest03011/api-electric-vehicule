d32352392a9fdefe974db3f601470c34335a8def
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-03 23:30:14 -0400

    Add a checker for the requests in order to confirm that the endpoint is allowed

--- public/index.php
+++ public/index.php
@@ -5,3 +5,15 @@ declare(strict_types=1);
 namespace App;
 
 require "../vendor/autoload.php";
+
+$parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+$endpoint = $parts[1];
+
+if ($endpoint !== "electric-vehicules") {
+  http_response_code(404);
+  echo json_encode(["message" => "Path not found"]);
+  exit;
+}
+
+echo json_encode(["message" => "Manuel, keep going. You are doing great!"]);
