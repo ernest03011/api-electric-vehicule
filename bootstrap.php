b72cdb67faa8f1bb67675ad10043e15f0afcfb81
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-03 23:45:31 -0400

    Add a bootstrap file to load dot env and eloquent ORM

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$capsule = new Capsule;
+$capsule->addConnection([
+  "driver" => $_ENV['DB_DRIVER'] ?? "mysql",
+  "host" => $_ENV['DB_DRIVER'] ?? "localhost",
+  "database" => $_ENV['DB_NAME'] ?? "",
+  "username" => $_ENV['DB_USERNAME'] ?? "root",
+  "password" => $_ENV['DB_PASSWORD'] ?? "",
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
