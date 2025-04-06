59d1fe3d115a1c5008264b026d33c93c79fc5c09
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-06 13:43:14 -0400

    Continue working with the methods that will call the Repository class depending on the requests

--- src/Controllers/ElectricVehicleController.php
+++ src/Controllers/ElectricVehicleController.php
@@ -12,13 +12,47 @@ class ElectricVehicleController
     protected ElectricVehicleRepository $electricVehicleRepository
   ) {}
 
-  public function processRequest(string $requestMethod, ?string $id)
+  public function processRequest(string $requestMethod, ?string $vin)
   {
-    $testingData = [
-      'method' => $requestMethod,
-      'id' => $id
-    ];
-    echo json_encode(["message" => $testingData]);
-    exit;
+
+    if ($vin) {
+      $response = $this->processResourceRequest($requestMethod, $vin);
+      echo json_encode(["message" => $response]);
+      return;
+    } else {
+      $response = $this->processCollectionRequest($requestMethod);
+      echo json_encode(["message" => $response]);
+    }
+  }
+
+  public function processResourceRequest(string $requestMethod, ?string $vin)
+  {
+
+    $vin = strtoupper($vin);
+    $requestMethod = strtolower($requestMethod);
+
+    if ($requestMethod === "patch") {
+      $data = (array) json_decode(file_get_contents("php://input"), true);
+    }
+
+    return match ($requestMethod) {
+      'get' => $this->electricVehicleRepository->find($vin),
+      'delete' => $this->electricVehicleRepository->delete($vin),
+      'patch' => $this->electricVehicleRepository->update($vin, $data),
+    };
+  }
+
+  public function processCollectionRequest(string $requestMethod)
+  {
+    $requestMethod = strtolower($requestMethod);
+
+    if ($requestMethod === "post") {
+      $data = (array) json_decode(file_get_contents("php://input"), true);
+    }
+
+    return match ($requestMethod) {
+      'get' => $this->electricVehicleRepository->findAll(),
+      'post' => $this->electricVehicleRepository->create($data),
+    };
   }
 }
