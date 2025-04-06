3f50d4ff1850443aa7c84ccba7baa1c20521b9ba
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-06 19:16:56 -0400

    Refactor the code in order to add try/catch block and add return types in order to have more control of the data that is returned and any errors when interacting with the model

--- src/Repositories/ElectricVehicleRepository.php
+++ src/Repositories/ElectricVehicleRepository.php
@@ -8,58 +8,82 @@ use App\Models\ElectricVehicle;
 
 class ElectricVehicleRepository
 {
-
-  public function findAll(int $limit = 10, int $offset = 0)
+  public function findAll(int $limit = 10, int $offset = 0): iterable | false
   {
-    return ElectricVehicle::select(
-      "VIN",
-      "Make",
-      "Model",
-      "Model_Year",
-      "Electric_Vehicle_Type",
-      "CAFV_Eligibility"
-    )->offset($offset)->limit($limit)->get();
+    try {
+      return ElectricVehicle::select(
+        "VIN",
+        "Make",
+        "Model",
+        "Model_Year",
+        "Electric_Vehicle_Type",
+        "CAFV_Eligibility"
+      )->offset($offset)->limit($limit)->get();
+    } catch (\Exception $e) {
+      return false;
+    }
   }
-  public function find($vin)
+  public function find(string $vin, ?bool $dataIsReturnedInArrayFormat = true): iterable
   {
-    $vehicle = ElectricVehicle::select(
-      "VIN",
-      "Make",
-      "Model",
-      "Model_Year",
-      "Electric_Vehicle_Type",
-      "CAFV_Eligibility"
-    )->where("VIN", $vin)->first();
+    try {
 
-    return $vehicle ? $vehicle->toArray() : [];
+      $vehicle = ElectricVehicle::select(
+        "VIN",
+        "Make",
+        "Model",
+        "Model_Year",
+        "Electric_Vehicle_Type",
+        "CAFV_Eligibility"
+      )->where("VIN", $vin)->first();
+
+      if ($dataIsReturnedInArrayFormat) {
+        return $vehicle ? $vehicle->toArray() : [];
+      }
+
+      return $vehicle;
+    } catch (\Exception $e) {
+      return [];
+    }
   }
-  public function delete($vin)
+  public function delete($vin): bool
   {
-    $vehicle = ElectricVehicle::find($vin);
-    $vehicle->delete();
-    $vehicle->refresh();
-    return "Vehicle with Vin: {$vin} has been deleted!";
+    try {
+      $vehicle = ElectricVehicle::find($vin);
+      $vehicle->delete();
+      $vehicle->refresh();
+      return true;
+    } catch (\Exception $e) {
+      return false;
+    }
   }
-  public function update($vin, $data)
+
+  public function update($vin, $data): bool
   {
+    try {
+      $vehicle = ElectricVehicle::find($vin);
+      $vehicle->Make = $data["Make"];
+      $vehicle->Model = $data["Model"];
+      $vehicle->Model_Year = $data["Model_Year"];
+      $vehicle->Electric_Vehicle_Type = $data["Electric_Vehicle_Type"];
+      $vehicle->CAFV_Eligibility = $data["CAFV_Eligibility"];
 
-    $vehicle = ElectricVehicle::find($vin);
-    $vehicle->Make = $data["Make"];
-    $vehicle->Model = $data["Model"];
-    $vehicle->Model_Year = $data["Model_Year"];
-    $vehicle->Electric_Vehicle_Type = $data["Electric_Vehicle_Type"];
-    $vehicle->CAFV_Eligibility = $data["CAFV_Eligibility"];
-
-    $vehicle->save();
-    $vehicle->refresh();
-
-    return $vehicle;
+      $vehicle->save();
+      $vehicle->refresh();
+      return true;
+    } catch (\Exception $e) {
+      return false;
+    }
   }
-  public function create($data)
+
+  public function create($data): string | false
   {
-    $vehicle = new ElectricVehicle;
-    $vehicle->fill($data);
-    $vehicle->save();
-    return $vehicle;
+    try {
+      $vehicle = new ElectricVehicle;
+      $vehicle->fill($data);
+      $vehicle->save();
+      return $vehicle->VIN;
+    } catch (\Exception $e) {
+      return false;
+    }
   }
 }
