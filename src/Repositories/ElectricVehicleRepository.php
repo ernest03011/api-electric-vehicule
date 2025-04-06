773106baf5a026b7dea379817d4a29fcae3ebd88
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-06 13:41:23 -0400

    Add the methods for the CRUD transactions which will interact with the data via the Model

--- src/Repositories/ElectricVehicleRepository.php
+++ src/Repositories/ElectricVehicleRepository.php
@@ -9,17 +9,57 @@ use App\Models\ElectricVehicle;
 class ElectricVehicleRepository
 {
 
-  public function findAll()
+  public function findAll(int $limit = 10, int $offset = 0)
   {
-    return ElectricVehicle::all();
+    return ElectricVehicle::select(
+      "VIN",
+      "Make",
+      "Model",
+      "Model_Year",
+      "Electric_Vehicle_Type",
+      "CAFV_Eligibility"
+    )->offset($offset)->limit($limit)->get();
   }
+  public function find($vin)
+  {
+    $vehicle = ElectricVehicle::select(
+      "VIN",
+      "Make",
+      "Model",
+      "Model_Year",
+      "Electric_Vehicle_Type",
+      "CAFV_Eligibility"
+    )->where("VIN", $vin)->first();
 
-  // $someVehicles = ElectricVehicle::select(
-  //   "VIN",
-  //   "Make",
-  //   "Model",
-  //   "Model_Year",
-  //   "Electric_Vehicle_Type",
-  //   "CAFV_Eligibility"
-  // )->offset(0)->limit(10)->get();
+    return $vehicle ? $vehicle->toArray() : [];
+  }
+  public function delete($vin)
+  {
+    $vehicle = ElectricVehicle::find($vin);
+    $vehicle->delete();
+    $vehicle->refresh();
+    return "Vehicle with Vin: {$vin} has been deleted!";
+  }
+  public function update($vin, $data)
+  {
+
+    $vehicle = ElectricVehicle::find($vin);
+    $vehicle->Make = $data["Make"];
+    $vehicle->Model = $data["Model"];
+    $vehicle->Model_Year = $data["Model_Year"];
+    $vehicle->Electric_Vehicle_Type = $data["Electric_Vehicle_Type"];
+    $vehicle->CAFV_Eligibility = $data["CAFV_Eligibility"];
+
+    $vehicle->save();
+    $vehicle->refresh();
+
+    return $vehicle;
+  }
+  public function create($data)
+  {
+    $vehicle = new ElectricVehicle;
+    $vehicle->fill($data);
+    $vehicle->save();
+    return $vehicle;
+  }
 }
