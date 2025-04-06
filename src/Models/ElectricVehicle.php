8d223a7a8e701b0021b67b46da68971915be6d1e
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-06 13:39:50 -0400

    Add the variables to disable the incrementing, the timestamps and adding the fillable columns for the table

--- src/Models/ElectricVehicle.php
+++ src/Models/ElectricVehicle.php
@@ -14,4 +14,28 @@ class ElectricVehicle extends Model
   protected $keyType = 'string';
 
   protected $primaryKey = 'VIN';
+
+  public $incrementing = false;
+
+  protected $fillable = [
+    'VIN',
+    'County',
+    'City',
+    'State',
+    'Postal_Code',
+    'Model_Year',
+    'Make',
+    'Model',
+    'Electric_Vehicle_Type',
+    'CAFV_Eligibility',
+    'Electric_Range',
+    'Base_MSRP',
+    'Legislative_District',
+    'DOL_Vehicle_ID',
+    'Vehicle_Location',
+    'Electric_Utility',
+    'Census_Tract'
+  ];
+
+  public $timestamps = false;
 }
