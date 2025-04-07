e7acd7db0b1f5c9d025a112b25e178020df23129
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-07 07:48:20 -0400

    PHPCS: Apply recommendations

--- bootstrap.php
+++ bootstrap.php
@@ -8,13 +8,15 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $capsule = new Capsule;
-$capsule->addConnection([
-  "driver" => $_ENV['DB_DRIVER'] ?? "mysql",
-  "host" => $_ENV['DB_HOST'] ?? "localhost",
-  "database" => $_ENV['DB_NAME'] ?? "",
-  "username" => $_ENV['DB_USERNAME'] ?? "root",
-  "password" => $_ENV['DB_PASSWORD'] ?? "",
-]);
+$capsule->addConnection(
+    [
+    "driver" => $_ENV['DB_DRIVER'] ?? "mysql",
+    "host" => $_ENV['DB_HOST'] ?? "localhost",
+    "database" => $_ENV['DB_NAME'] ?? "",
+    "username" => $_ENV['DB_USERNAME'] ?? "root",
+    "password" => $_ENV['DB_PASSWORD'] ?? "",
+    ]
+);
 
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
