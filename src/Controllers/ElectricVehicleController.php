d0c6c31143247ce43ef408e3128526b97b5cd2ef
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-07 07:48:42 -0400

    PHPCS: Apply recommendations

--- src/Controllers/ElectricVehicleController.php
+++ src/Controllers/ElectricVehicleController.php
@@ -4,204 +4,212 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Repositories\ElectricVehicleRepository;
 use App\Services\HttpMessageService;
+use App\Repositories\ElectricVehicleRepository;
 
 class ElectricVehicleController
 {
-  private int $httpResponseCode = 200;
-  private string $httpResponseHeader = "";
-  private array $httpResponseMessage = [];
+    private int $httpResponseCode = 200;
+    private string $httpResponseHeader = "";
+    private array $httpResponseMessage = [];
 
-  public function __construct(
-    protected ElectricVehicleRepository $electricVehicleRepository
-  ) {}
-
-  public function processRequest(string $requestMethod, ?string $vin)
-  {
-
-    if ($vin) {
-      $this->processResourceRequest($requestMethod, $vin);
-    } else {
-      $this->processCollectionRequest($requestMethod);
+    public function __construct(
+        protected ElectricVehicleRepository $electricVehicleRepository
+    ) {
     }
 
-    HttpMessageService::response(
-      $this->httpResponseMessage,
-      $this->httpResponseCode,
-      $this->httpResponseHeader
-    );
-    exit;
-  }
+    public function processRequest(string $requestMethod, ?string $vin)
+    {
 
-  public function processResourceRequest(string $requestMethod, ?string $vin)
-  {
+        if ($vin) {
+            $this->processResourceRequest($requestMethod, $vin);
+        } else {
+            $this->processCollectionRequest($requestMethod);
+        }
 
-    $vin = strtoupper($vin);
-    $requestMethod = strtolower($requestMethod);
-
-    $vehicle = $this->electricVehicleRepository->find($vin);
-
-    if (count($vehicle) <= 0) {
-      HttpMessageService::response(
-        ["message" => "Vehicle with this VIN {$vin} was not found"],
-        404,
-      );
-      return;
-    }
-
-    if ($requestMethod === "patch") {
-      $data = (array) json_decode(file_get_contents("php://input"), true);
-
-      $errors = $this->getValidationErrors($data, false);
-
-      if (! empty($errors)) {
         HttpMessageService::response(
-          ["errors" => $errors],
-          422,
-          ""
+            $this->httpResponseMessage,
+            $this->httpResponseCode,
+            $this->httpResponseHeader
         );
         exit;
-      }
     }
 
-    match ($requestMethod) {
-      'get' => (function () use ($vehicle) {
-        $this->httpResponseMessage = ["message" => $vehicle];
-      })(),
-      'delete' => (function () use ($vin) {
-        $response = $this->electricVehicleRepository->delete($vin);
+    public function processResourceRequest(string $requestMethod, ?string $vin)
+    {
 
-        if (! $response) {
-          $this->httpResponseCode = 500;
-          $this->httpResponseMessage = ["message" => "Something went wrong, try again!"];
-          return;
+        $vin = strtoupper($vin);
+        $requestMethod = strtolower($requestMethod);
+
+        $vehicle = $this->electricVehicleRepository->find($vin);
+
+        if (count($vehicle) <= 0) {
+            HttpMessageService::response(
+                ["message" => "Vehicle with this VIN {$vin} was not found"],
+                404,
+            );
+            return;
         }
 
-        $this->httpResponseMessage = ["message" => "Vehicle with Vin number {$vin} has been deleted"];
-      })(),
-      'patch' => (function () use ($vin, $data) {
-        $response = $this->electricVehicleRepository->update($vin, $data);
+        if ($requestMethod === "patch") {
+            $data = (array) json_decode(file_get_contents("php://input"), true);
 
-        if (! $response) {
-          $this->httpResponseCode = 500;
-          $this->httpResponseMessage = ["message" => "Something went wrong, try again!"];
-          return;
+            $errors = $this->getValidationErrors($data, false);
+
+            if (! empty($errors)) {
+                HttpMessageService::response(
+                    ["errors" => $errors],
+                    422,
+                    ""
+                );
+                exit;
+            }
         }
 
-        $this->httpResponseMessage = ["message" => "Vehicle with Vin number {$vin} has been updated"];
-      })(),
-      default => (function () {
-        HttpMessageService::response(
-          [],
-          405,
-          "Allow: GET, PATCH, DELETE"
-        );
-        exit;
-      })(),
-    };
-  }
+        match ($requestMethod) {
+            'get' => (function () use ($vehicle) {
+                $this->httpResponseMessage = ["message" => $vehicle];
+            })(),
+            'delete' => (function () use ($vin) {
+                $response = $this->electricVehicleRepository->delete($vin);
 
-  public function processCollectionRequest(string $requestMethod)
-  {
-    $requestMethod = strtolower($requestMethod);
+                if (! $response) {
+                    $this->httpResponseCode = 500;
+                    $this->httpResponseMessage =    
+                    ["message" => "Something went wrong, try again!"];
+                    return;
+                }
 
-    if ($requestMethod === "post") {
-      $data = (array) json_decode(file_get_contents("php://input"), true);
+                $this->httpResponseMessage = 
+                ["message" => "Vehicle with Vin number {$vin} has been deleted"];
+            })(),
+            'patch' => (function () use ($vin, $data) {
+                $response = $this->electricVehicleRepository->update($vin, $data);
+
+                if (! $response) {
+                    $this->httpResponseCode = 500;
+                    $this->httpResponseMessage = 
+                    ["message" => "Something went wrong, try again!"];
+                    return;
+                }
+
+                $this->httpResponseMessage = 
+                ["message" => "Vehicle with Vin number {$vin} has been updated"];
+            })(),
+            default => (function () {
+                HttpMessageService::response(
+                    [],
+                    405,
+                    "Allow: GET, PATCH, DELETE"
+                );
+                exit;
+            })(),
+        };
     }
 
-    match ($requestMethod) {
-      'get' => (function () {
-        $response = $this->electricVehicleRepository->findAll();
+    public function processCollectionRequest(string $requestMethod)
+    {
+        $requestMethod = strtolower($requestMethod);
 
-        if (! $response) {
-          $this->httpResponseCode = 500;
-          $this->httpResponseMessage = ["message" => "Something went wrong, try again!"];
-          return;
+        if ($requestMethod === "post") {
+            $data = (array) json_decode(file_get_contents("php://input"), true);
         }
 
-        $this->httpResponseMessage = ["message" => $response];
-      })(),
-      'post' => (function () use ($data) {
-        $vin = $this->electricVehicleRepository->create($data);
+        match ($requestMethod) {
+            'get' => (function () {
+                $response = $this->electricVehicleRepository->findAll();
 
-        if (! $vin) {
-          $this->httpResponseCode = 500;
-          $this->httpResponseMessage = ["message" => "Something went wrong, try again!"];
-          return;
+                if (! $response) {
+                    $this->httpResponseCode = 500;
+                    $this->httpResponseMessage = 
+                    ["message" => "Something went wrong, try again!"];
+                    return;
+                }
+
+                $this->httpResponseMessage = ["message" => $response];
+            })(),
+            'post' => (function () use ($data) {
+                $vin = $this->electricVehicleRepository->create($data);
+
+                if (! $vin) {
+                    $this->httpResponseCode = 500;
+                    $this->httpResponseMessage = 
+                    ["message" => "Something went wrong, try again!"];
+                    return;
+                }
+
+                $this->httpResponseCode = 201;
+                $this->httpResponseMessage = 
+                ["message" => "Vehicle with Vin number {$vin} has been created"];
+            })(),
+            default => (function () {
+                HttpMessageService::Response(
+                    [],
+                    405,
+                    "Allow: GET, POST"
+                );
+                exit;
+            })(),
+        };
+    }
+
+    private function getValidationErrors(array $data, ?bool $isItNew = true)
+    {
+        $updateArrayExample = [
+        'Make' => 'testing34',
+        'Model' => 'testing34',
+        'Model_Year' => 2021,
+        'Electric_Vehicle_Type' => 'testing34',
+        'CAFV_Eligibility' => 'testing34',
+        ];
+
+        $newArrayExample = [
+        "VIN" => "1G1FZ6S05L4109876",
+        "County" => "Spokane",
+        "City" => "Spokane",
+        "State" => "WA",
+        "Postal_Code" => "99201",
+        "Model_Year" => 2021,
+        "Make" => "Chevrolet",
+        "Model" => "Bolt EV",
+        "Electric_Vehicle_Type" => "Battery Electric Vehicle (BEV)",
+        "CAFV_Eligibility" => "Eligible",
+        "Electric_Range" => 259,
+        "Base_MSRP" => 36620,
+        "Legislative_District" => 3,
+        "DOL_Vehicle_ID" => 567891234,
+        "Vehicle_Location" => "47.6588,-117.4260",
+        "Electric_Utility" => "Avista",
+        "Census_Tract" => "53063001500"
+        ];
+
+        $errors = [];
+
+        $keysFromInputData = array_keys($data);
+        sort($keysFromInputData);
+
+
+        if ($isItNew) {
+            $keysFromNewArrayExample = array_keys($newArrayExample);
+            sort($keysFromNewArrayExample);
+
+            if ($keysFromInputData !== $keysFromNewArrayExample) {
+                $errors[] = "Data is incorrect";
+            }
+
+            return;
         }
 
-        $this->httpResponseCode = 201;
-        $this->httpResponseMessage = ["message" => "Vehicle with Vin number {$vin} has been created"];
-      })(),
-      default => (function () {
-        HttpMessageService::Response(
-          [],
-          405,
-          "Allow: GET, POST"
-        );
-        exit;
-      })(),
-    };
-  }
-
-  private function getValidationErrors(array $data, ?bool $isItNew = true)
-  {
-    $updateArrayExample = [
-      'Make' => 'testing34',
-      'Model' => 'testing34',
-      'Model_Year' => 2021,
-      'Electric_Vehicle_Type' => 'testing34',
-      'CAFV_Eligibility' => 'testing34',
-    ];
-
-    $newArrayExample = [
-      "VIN" => "1G1FZ6S05L4109876",
-      "County" => "Spokane",
-      "City" => "Spokane",
-      "State" => "WA",
-      "Postal_Code" => "99201",
-      "Model_Year" => 2021,
-      "Make" => "Chevrolet",
-      "Model" => "Bolt EV",
-      "Electric_Vehicle_Type" => "Battery Electric Vehicle (BEV)",
-      "CAFV_Eligibility" => "Eligible",
-      "Electric_Range" => 259,
-      "Base_MSRP" => 36620,
-      "Legislative_District" => 3,
-      "DOL_Vehicle_ID" => 567891234,
-      "Vehicle_Location" => "47.6588,-117.4260",
-      "Electric_Utility" => "Avista",
-      "Census_Tract" => "53063001500"
-    ];
-
-    $errors = [];
-
-    $keysFromInputData = array_keys($data);
-    sort($keysFromInputData);
+        if (! $isItNew) {
+            $keysFromUpdateArrayExample = array_keys($updateArrayExample);
+            sort($keysFromUpdateArrayExample);
 
 
-    if ($isItNew) {
-      $keysFromNewArrayExample = array_keys($newArrayExample);
-      sort($keysFromNewArrayExample);
+            if ($keysFromInputData !== $keysFromUpdateArrayExample) {
+                $errors[] = "Data is incorrect";
+            }
 
-      if ($keysFromInputData !== $keysFromNewArrayExample) {
-        $errors[] = "Data is incorrect";
-      }
-
-      return;
+            return $errors;
+        }
     }
-
-    if (! $isItNew) {
-      $keysFromUpdateArrayExample = array_keys($updateArrayExample);
-      sort($keysFromUpdateArrayExample);
-
-
-      if ($keysFromInputData !== $keysFromUpdateArrayExample) {
-        $errors[] = "Data is incorrect";
-      }
-
-      return $errors;
-    }
-  }
 }
