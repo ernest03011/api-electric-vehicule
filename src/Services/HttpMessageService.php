eb49b9978c8ca751785da83feef78e5b0dd59477
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-04-07 08:02:35 -0400

    PHPSTAN: Apply recommendations

--- src/Services/HttpMessageService.php
+++ src/Services/HttpMessageService.php
@@ -6,7 +6,7 @@ namespace App\Services;
 
 class HttpMessageService
 {
-    public static function response(array $body = [], int $code = 200, ?string $headerBody = null)
+    public static function response(array $body = [], int $code = 200, ?string $headerBody = ""): void
     {
         if ($headerBody) {
             http_response_code($code);
